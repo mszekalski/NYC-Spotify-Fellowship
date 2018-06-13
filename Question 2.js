b05a3c6126db19df9c0ba32ea1505785f8b0a075
Matthew Szekalski <mattszekalski@icloud.com>
2018-06-13 12:30:29 -0400

    finished question 2

--- Question 2.js
+++ Question 2.js
@@ -1,8 +1,23 @@
 function decodeString(s) {
   let array = [];
+  let numbers = "0123456789";
   for (let i = 0; i < s.length; i++) {
     if (s[i] !== "]") {
       array.push(s[i]);
+    } else {
+      let temp = "";
+      let count = "";
+      while (array[array.length - 1] !== "[") {
+        temp = array.pop() + temp;
+      }
+      array.pop();
+      while (numbers.includes(array[array.length - 1])) {
+        count = array.pop() + count;
+      }
+      count = Number(count);
+      temp = temp.repeat(count);
+      array.push(temp);
     }
   }
+  return array.join("");
 }
