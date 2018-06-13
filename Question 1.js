b112afa74968ef9f585c9ba82f69424985a5c04f
Matthew Szekalski <mattszekalski@icloud.com>
2018-06-12 21:26:41 -0400

    finished solution for Question 1

---
+++ Question 1.js
@@ -0,0 +1,30 @@
+function sortByStrings(s, t) {
+  let object = {};
+  let newString = "";
+  for (let i = 0; i < s.length; i++) {
+    if (object[s[i]] === undefined) {
+      object[s[i]] = 1;
+    } else {
+      object[s[i]] += 1;
+    }
+  }
+  for (let i = 0; i < t.length; i++) {
+    newString += t[i].repeat(object[t[i]]);
+  }
+  return newString;
+}
+
+function sortByStrings(s, t) {
+  s = s.split("").sort(function(a, b) {
+    return t.indexOf(a) - t.indexOf(b);
+  });
+  return s.join("");
+}
+
+function sortByStrings(s, t) {
+  s = s.split("");
+  s.sort(function(a, b) {
+    return t.indexOf(a) - t.indexOf(b);
+  });
+  return s.join("");
+}
---
+++ Question 3.js
@@ -0,0 +1,1 @@
+function changePossibilities(amount, amount) {}
