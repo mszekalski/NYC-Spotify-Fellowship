1b3169010a48b82ed9ffd4427e6e2175c7115747
Matthew Szekalski <mattszekalski@icloud.com>
2018-06-12 21:53:04 -0400

    adjusted question 1

--- Question 1.js
+++ Question 1.js
@@ -15,16 +15,10 @@ function sortByStrings(s, t) {
 }
 
 function sortByStrings(s, t) {
-  s = s.split("").sort(function(a, b) {
-    return t.indexOf(a) - t.indexOf(b);
-  });
-  return s.join("");
-}
-
-function sortByStrings(s, t) {
-  s = s.split("");
-  s.sort(function(a, b) {
-    return t.indexOf(a) - t.indexOf(b);
-  });
-  return s.join("");
+  return s
+    .split("")
+    .sort(function(a, b) {
+      return t.indexOf(a) - t.indexOf(b);
+    })
+    .join("");
 }
